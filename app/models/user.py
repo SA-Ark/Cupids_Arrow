21fd10dc0de2b2ce992575140f84b075c794695b
Arko Chakrabarty <c.arko.business@gmail.com>
2023-01-13 19:16:00 -0800

    finished Users

--- app/models/user.py
+++ app/models/user.py
@@ -11,8 +11,33 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    relationship_status = db.Column(db.String(40), nullable=False)
+    city = db.Column(db.String(40), nullable=False)
+    state = db.Column(db.String(40), nullable=False)
+
+    biography = db.Column(db.String(40))
+    gender = db.Column(db.String(40))
+    sexual_orientation = db.Column(db.String(40))
+    income = db.Column(db.Integer)
+    kids = db.Column(db.Integer)
+    relationship_goal = db.Column(db.String(40))
+    race = db.Column(db.String(40))
+    height = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    inhabitants = db.Column(db.Boolean)
+    religion = db.Column(db.String(40))
+    premium = db.Column(db.Boolean)
+    desired_profile = db.relationship("DesiredPartnerAttributes", back_populates="id")
+    created = db.Column(db.Date)
+    #  id = db.relationship("User", back_populates="desired_profile", primary_key=True)
+
+
+
+
 
     @property
     def password(self):

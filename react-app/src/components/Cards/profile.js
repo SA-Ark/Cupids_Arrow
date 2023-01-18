d5b09d42699da518eaa694aa6a1a1be8cc5900e0
mauroalvarez <mauro.alvarez.jr0@gmail.com>
2023-01-18 12:02:12 -0600

    Cards html

--- react-app/src/components/Cards/profile.js
+++ react-app/src/components/Cards/profile.js
@@ -93,16 +93,134 @@ function SpotCard({ answers, match, place }) {
 
 
                 <div id='bottomproLEFTcol'>
-
-                </div>
-
-                <div id='bottomproRIGHTcol'>
-                    <div id='match'>
-
+                    {/* for loop through filled out cards to display start*/}
+                   <div id="sumamryCard">
+                    <div id="summaryCardBlackBar">
+                        <h3 id="summaryCardTitle"></h3>
                     </div>
+                    <div id="summaryCardWhiteBoxColumn">
+                        <div id="summaryCardWhiteContent"></div>
+                        <div id="summaryCardWhiteBoxmessage"></div>
+                    </div>
+                   </div>
+                   {/* for loop through filled out cards to display end*/}
+                   {/* question percent and results card start*/}
+                   <div id="sumamryCard">
+                    <div id="summaryCardBlackBar">
+                        <h3 id="summaryCardTitle"></h3>
+                    </div>
+                    <div id="questionsCardWhiteBoxRow">
+                        <div id="questionsCardPicture%Left">
+                            <div class="profile-questions-circles">
+                                <div class="profile-questions-userinfo-photos">
+                                    <div class="profile-questions-userinfo-photos-photo">my image</div>
+                                    <div class="profile-questions-userinfo-photos-photo">other persons image</div>
+                                </div>
+                                <div class="match-percent-pink-circle"></div>
+                            </div>
+                        </div>
+                        <div id="questionsCard-ADF-RightColumn">
+                            <div id='agree' class="profile-questions-entry-filters">
+                                <div class="profile-questions-entry-filters-left">
+                                    <div class="profile-questions-entry-filters-title"></div>
+                                    <div class="profile-questions-entry-filters-icon"></div>
+                                </div>
+                                <div class="profile-questions-entry-filters-right">
+                                    <div class="profile-questions-entry-filters-count"></div>
+                                </div>
+                            </div>
+                            <div id='disagree' class="profile-questions-entry-filters">
+                                <div class="profile-questions-entry-filters-left">
+                                    <div class="profile-questions-entry-filters-title"></div>
+                                    <div class="profile-questions-entry-filters-icon"></div>
+                                </div>
+                                <div class="profile-questions-entry-filters-right">
+                                    <div class="profile-questions-entry-filters-count"></div>
+                                </div>
+                            </div>
+                            <div id='findOut' class="profile-questions-entry-filters">
+                                <div class="profile-questions-entry-filters-left">
+                                    <div class="profile-questions-entry-filters-title"></div>
+                                    <div class="profile-questions-entry-filters-icon"></div>
+                                </div>
+                                <div class="profile-questions-entry-filters-right">
+                                    <div class="profile-questions-entry-filters-count"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   </div>
+                   {/* question percent and results card end*/}
+                </div>
                 
-                    <>
-                    </>
+                <div id='bottomproRIGHTcol'>
+                    <div id="sumamryCard" className='details'>
+                        <div id="summaryCardBlackBar">
+                            <h3 id="summaryCardTitle"></h3>
+                        </div>
+                        <div id="summaryCardWhiteBoxColumn">
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                            <div id='details-info-row-column'>
+                                <div id="details-icon"></div>
+                                <div id="details-icon-info"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="sumamryCard">
+                        <div id="summaryCardBlackBar">
+                            <h3 id="summaryCardTitle"></h3>
+                        </div>
+                        <div id="summaryCardWhiteBoxColumn">
+                            <div id="summaryCardWhiteContentRow">
+                                <div id="summaryCardWhiteContentRowLeft">
+                                    <div id='last-active-icon'></div>
+                                    <div id='last-active-title'></div>
+                                </div>
+                                <div id="summaryCardWhiteContentRowRight"></div>
+                            </div>
+                            <div id="summaryCardWhiteContentRow">
+                                <div id="summaryCardWhiteContentRowLeft">
+                                    <div id='reply-time-icon'></div>
+                                    <div id='reply-time-title'></div>
+                                </div>
+                                <div id="summaryCardWhiteContentRowRight"></div>
+                            </div>
+                            <div id="summaryCardWhiteContentRow">
+                                <div id="summaryCardWhiteContentRowLeft">
+                                    <div id='reply-likely-icon'></div>
+                                    <div id='reply-likely-title'></div>
+                                </div>
+                                <div id="summaryCardWhiteContentRowRight"></div>
+                            </div>
+                            <div id="summaryCardWhiteContentRow">
+                                <div id="summaryCardWhiteContentRowLeft">
+                                    <div id='sends-first-icon'></div>
+                                    <div id='sends-first-title'></div>
+                                </div>
+                                <div id="summaryCardWhiteContentRowRight"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
 

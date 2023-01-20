1d9b315dcdb37c099633e8a2cffb32cfe20429c4
Arko <90512504+SA-Ark@users.noreply.github.com>
2023-01-20 08:21:00 -0800

    Arko (#11)
    * finished Users
    
    * models, first test
    
    * redid migration
    
    * committed alembic
    
    * routes
    
    * Routes
    
    * sunday night push
    
    * minor changes
    
    * some models errors fixed before lunch
    
    * fixed model issues
    
    * Profile Routes, added full crud for images
    
    * sign up is finished
    
    * Profile Routes, added full crud for images fixed
    
    * fixed seeder issues
    
    * EOD Monday
    
    * python version 3.9.4 pipfile dex debug
    
    * models
    
    * Image routes and forms
    
    * file structure and light work
    
    * front end changes to folders and signupform
    
    * Arko and mauro merged
    
    * Dev fullly merged
    
    * card for profile almost done
    
    * modals setup done template not so much
    
    * basic file struture update and reducer
    
    * light reducer work
    
    * Cards html
    
    * MyProfile, added questions box try 2
    
    * question/profile pages
    
    * fixed redux state errors and did some more redux stuff
    
    * add
    
    * new commit
    
    * Added seed data
    
    * fixed added seed data
    
    * finished more than half states
    
    * routes for testing
    
    * fixed added seed data
    
    * finished another redux store
    
    * fixed added seed data
    
    * finished redux state rough draft
    
    * tinkering on seeds and questions component
    
    * q route
    
    * finished 2 more  seeds
    
    * finished debugging seed data
    
    * fixed some bugs
    
    * finished seed ata and debugging for questions route. still need backend route and frontend form
    
    * ready for form fixes
    
    * form completion for questions, next css
    
    * finished question functionality
    
    * typo
    
    * eliminated duplicates
    
    * create state fix
    
    * meow
    
    * questioons put thuink and frontend done
    
    * fixed questions form data and front-end logic
    
    * merged prap
    
    * routes for images and thunk finalized, need bug testing
    
    * images
    
    * url
    
    * fixed image post
    
    * deleted console.log
    
    * some light work
    
    * current user images been thunked
    
    * checking
    
    * orig migrations
    
    * deleted dev
    
    * deleted dev
    
    * foreign key render
    
    * merged
    
    * migration fix
    
    * import os
    
    Co-authored-by: mauroalvarez <mauro.alvarez.jr0@gmail.com>
    Co-authored-by: Prapassorn Tinnabavorn <Ptinnabavorn@yahoo.com>
    Co-authored-by: P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>

--- migrations/versions/ad8148f33c68_.py
+++ migrations/versions/ad8148f33c68_.py
@@ -12,8 +12,6 @@ import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
-environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.

dd2c48e7a3ec38bec42509c1195da50af8c0b483
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2023-01-21 17:35:39 -0800

    going through merge

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-
+import { useHistory } from 'react-router-dom'
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LoginForm from '../auth/LoginForm';
@@ -7,59 +7,57 @@ import OpenModalButton from '../OpenModalButton'
 import './NavBar.css'
 
 const NavBar = () => {
+  const history = useHistory()
+
   return (
     <nav className='nav-bar-container'>
       <div>
-        <ul>
-          <li>
-            <NavLink to='/' exact={true} activeClassName='active'>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            {/* <NavLink to='/login' exact={true} activeClassName='active'>
+
+
+        <li>
+          {/* <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink> */}
-            < OpenModalButton
-              id='createreviewbutt'
-              buttonText="Login!"
-              modalComponent={<LoginForm />}
-            />
-          </li>
-          <li>
-            <NavLink to='/sign-up' exact={true} activeClassName='active'>
-              Sign Up
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/users' exact={true} activeClassName='active'>
-              Users
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/discover' exact={true} activeClassName='active'>
-              Discover
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/questions' exact={true} activeClassName='active'>
-              Q's
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/myimages' exact={true} activeClassName='active'>
-              Images
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/profile' exact={true} activeClassName='active'>
-              My Profile
-            </NavLink>
-          </li>
-          <li>
-            <LogoutButton />
-          </li>
-        </ul>
+          < OpenModalButton
+            id='createreviewbutt'
+            buttonText="Login!"
+            modalComponent={<LoginForm />}
+          />
+        </li>
+        <li>
+          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+            Sign Up
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/users' exact={true} activeClassName='active'>
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/discover' exact={true} activeClassName='active'>
+            Discover
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/questions' exact={true} activeClassName='active'>
+            Q's
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/myimages' exact={true} activeClassName='active'>
+            Images
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/profile' exact={true} activeClassName='active'>
+            My Profile
+          </NavLink>
+        </li>
+        <li>
+          <LogoutButton />
+        </li>
+
       </div>
     </nav>
   );

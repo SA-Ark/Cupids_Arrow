da6fd6a35c73da86be3ef4d711c24b479cc33523
P. Dexter Assaf <107891735+D0RK5TER@users.noreply.github.com>
2023-01-22 16:48:49 -0800

    modals mostly done

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -12,30 +12,30 @@ const NavBar = () => {
   const history = useHistory()
   const user = useSelector(state => state.user)
   const img = useSelector(state => state.images)
-  const [hide, setHide] = useState(0)
+  const [hide, setHide] = useState(false)
   // const [session, setSession] = useState('none')
   // const [nosesh, setNosesh] = useState('flex')
 
   // const hider = (hide) => setHide(hide + 1)
 
-  let profilepic
-  let navright2
-  let navright
-  if (user?.id) {
-    navright = 'none'
-    navright2 = 'flex'
-  }
-  else {
-    navright2 = 'none'
-    navright = 'flex'
-  }
-  useEffect(() => {
-    
-  }, [user])
+  // let profilepic
+  // let navright2
+  // let navright
+  // if (user?.id) {
+  //   navright = 'none'
+  //   navright2 = 'flex'
+  // }
+  // else {
+  //   navright2 = 'none'
+  //   navright = 'flex'
+  // }
+  // useEffect(() => {
+
+  // }, [user])
 
 
   return (
-    <nav className='nav-bar-container' style={{backgroundColor: 'pink'}}>
+    <div className='nav-bar-container' style={{ backgroundColor: 'black', color: 'white' }}>
 
       <div id='navbarleft'>
         <div id='homeiconbutton'
@@ -47,13 +47,31 @@ const NavBar = () => {
 
       <div id='navbarmid'>
         <div id='midbuttonleft'>
+          <button
+            onClick={() => history.push('/discover')}
+            style={{ display: user?.id ? 'flex' : 'none' }}
+          > Discover</button>
+        </div>
 
-        </div>
-        {navright}
+
         <div id='midbuttonmid'>
+          <button
+            onClick={() => history.push('/questions')}
+            style={{ display: user?.id ? 'flex' : 'none' }}
+          >Questions</button>
         </div>
-        {navright2}
+
+
         <div id='midbuttonright'>
+          <button
+            onClick={() => history.push('/likes')}
+            style={{ display: user?.id ? 'flex' : 'none' }}
+          > Likes</button>
+        </div>
+        <div style={{ display: user?.id ? 'flex' : 'none' }}
+        >
+          <LogoutButton style={{ display: user?.id ? 'flex' : 'none' }}
+          />
         </div>
       </div>
 
@@ -66,7 +84,7 @@ const NavBar = () => {
 
 
       <div id='navbarright'>
-        <div id='togglenavsignlog' style={{ display:  navright2  }}>
+        <div id='togglenavsignlog' style={{ display: user?.id ? 'none' : 'flex' }}>
           <div>
             < OpenModalButton
               id='createreviewbutt'
@@ -82,17 +100,18 @@ const NavBar = () => {
             />
           </div>
         </div>
-
-        <div id='togglenavprofile' style={{ display: navright }}>
+        <div id='togglenavprofile' style={{ display: user?.id ? 'flex' : 'none' }
+        }>
           <div id='navbarprofile'
             onClick={() => history.push('/profile')}
-            style={{ backgroundImage: `url("${profilepic}")` }}
+          // style={{ backgroundImage: `url("${profilepic}")` }}
           >
           </div>
           <p>{user?.first_name}</p>
         </div>
       </div>
 
+    </div>
 
 
 
@@ -106,67 +125,6 @@ const NavBar = () => {
 
 
 
-
-      <div className='nav-bar-two'>
-        <ul>
-          <li>
-            <NavLink to='/' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Home
-            </NavLink>
-          </li>
-          <li className="button-nav">
-            {/* <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink> */}
-            < OpenModalButton
-              id='createreviewbutt'
-              buttonText="Login!"
-              modalComponent={<LoginForm />}
-            />
-          </li>
-          <li>
-            < OpenModalButton
-              id='createreviewbutt'
-              buttonText="Sign Up"
-              modalComponent={<SignUpForm />}
-            />
-          </li>
-          {/* <li>
-            <NavLink to='/users' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Users
-            </NavLink>
-          </li> */}
-          <li>
-            <NavLink to='/discover' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Discover
-            </NavLink>
-          </li>
-          {/* <li>
-            <NavLink to='/questions' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Q's
-            </NavLink>
-          </li> */}
-          <li>
-            <NavLink to='/likes' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Likes
-            </NavLink>
-          </li>
-          {/* <li>
-            <NavLink to='/myimages' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              Images
-            </NavLink>
-          </li> */}
-          {/* <li>
-            <NavLink to='/profile' exact={true} activeClassName='active' style={{ textDecoration: 'none', color: 'white' }}>
-              My Profile
-            </NavLink>
-          </li> */}
-          <li className="button-nav">
-            <LogoutButton />
-          </li>
-        </ul>
-      </div>
-    </nav>
   );
 }
 
